b522768264d36af2c566b2e52b30f1a63cd4c2d4
Pouya Kary <pouya@kary.us>
2022-11-05 22:08:45 +0330

    nothing to show indicator

--- src/web/actions/addCursorToAllNeighborLines.ts
+++ src/web/actions/addCursorToAllNeighborLines.ts
@@ -1,11 +1,27 @@
-import * as kit from '../kit';
+import * as kit    from '../kit';
+import * as vscode from 'vscode';
 
 // ─── Executer ──────────────────────────────────────────────────────────── ✣ ─
 
 export async function addCursorToAllNeighborLines() {
-  const range = kit.Columns.neighborLinesOfCurrentRenderColumn;
-  if (range === null) {
+  const linesRange = kit.Columns.neighborLinesOfCurrentRenderColumn;
+  if (linesRange === null || linesRange[0] === linesRange[1]) {
+    showNothingToSelectIndicator();
     return;
   }
-  kit.Columns.putCursorsInLinesRangeWithCurrentColumn(...range);
+  kit.Columns.putCursorsInLinesRangeWithCurrentColumn(...linesRange);
+}
+
+// ─── Nothing To Select Indicator ───────────────────────────────────────── ✣ ─
+
+function showNothingToSelectIndicator() {
+  const showTime      = 400;
+  const statusbarItem = vscode.window.createStatusBarItem();
+  statusbarItem.text  = "●";
+  statusbarItem.show();
+
+  setTimeout(() => {
+    statusbarItem.hide();
+    statusbarItem.dispose();
+  }, showTime);
 }
