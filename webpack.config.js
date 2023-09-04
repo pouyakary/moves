bd401b0227996cdd722af42006ecbc9d818cc3a9
Pouya Kary ✨ <kary@gnu.org>
2023-09-05 01:16:13 +0330

    removed the testing stuff

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,6 @@ const webExtensionConfig = {
 	target: 'webworker', // extensions run in a webworker context
 	entry: {
 		'extension': './src/web/extension.ts',
-		'test/suite/index': './src/web/test/suite/index.ts'
 	},
 	output: {
 		filename: '[name].js',
